7f77c262d07ac694825584400b2eee35f8093645
Kanjurito <robin.maxime71@gmail.com>
2026-02-21 10:02:02 +0100

    add wallpaper script

---
+++ niri/script/wallpaper_carousel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Dossier des images (bien mis entre guillemets à cause des espaces)
+WALLPAPER_DIR="/run/media/alterra/Steam_epic/Fond d'ecran"
+
+# Temps entre chaque image (en secondes) - ici 300s = 5 minutes
+INTERVAL=300
+
+while true; do
+    # On choisit une image au hasard
+    IMAGE=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+    
+    # On l'applique avec swww (avec un effet de transition sympa)
+    swww img "$IMAGE" --transition-type outer --transition-step 30
+    
+    sleep $INTERVAL
+done
